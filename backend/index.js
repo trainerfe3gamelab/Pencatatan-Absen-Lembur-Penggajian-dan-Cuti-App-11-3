4b7d89c6d4e85313cf273585e7fefc3949e5aafe
bach <bachtiaralif41@gmail.com>
2024-05-29 15:20:04 +0700

    feat: menambahkan attendance routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 const positionRoutes = require("./routes/positionRoutes");
+const attendanceRoutes = require("./routes/attendanceRoutes");
 const app = express();
 
 // Middleware
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", positionRoutes);
+app.use("/api", attendanceRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
