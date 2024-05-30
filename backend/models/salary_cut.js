244da4fd6877251b0e8ebb74f15ded7d261595bc
bach <bachtiaralif41@gmail.com>
2024-05-30 15:02:34 +0700

    feat: menambahkan model salary_cut

---
+++ backend/models/salary_cut.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const SalaryCut = sequelize.define(
+    "SalaryCut",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      cut: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      archived: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      creation_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      update_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      update_id: {
+        type: DataTypes.UUID,
+        allowNull: true,
+      },
+      create_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: "tbl_salary_cut",
+    }
+  );
+
+  return SalaryCut;
+};
