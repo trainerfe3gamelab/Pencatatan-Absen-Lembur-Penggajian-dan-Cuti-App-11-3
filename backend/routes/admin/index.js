76fa19c0be0929a21cfc6504d1aa04f7e686974a
bach <bachtiaralif41@gmail.com>
2024-05-31 09:30:43 +0700

    feat: menambahkan salaryCutRoutes pada router

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -3,10 +3,12 @@ const { adminMiddleware } = require("../../utils/middleware/auth");
 const positionRoutes = require("./positionRoutes");
 const overtimeRoutes = require("./overtimeRoutes");
 const attendanceRoutes = require("./attendanceRoutes");
+const salaryCutRoutes = require("./salaryCutRoutes");
 
 router.use(adminMiddleware);
 router.use("/position", positionRoutes);
 router.use("/overtimes", overtimeRoutes);
 router.use("/attendances", attendanceRoutes);
+router.use("/salarycuts", salaryCutRoutes);
 
 module.exports = router;
