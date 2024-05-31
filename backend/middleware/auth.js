fed5c3e227ac18042ea27e203c0479093dd450f3
bach <bachtiaralif41@gmail.com>
2024-05-31 10:02:16 +0700

    fix: memperbaiki path dari model

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../../models");
+const { User } = require("../models");
 
 const verifyTokenMiddleware = async (req, res, next) => {
   const authorization = req.header("Authorization");
