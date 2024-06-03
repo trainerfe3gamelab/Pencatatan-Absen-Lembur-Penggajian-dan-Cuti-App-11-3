53b315e75a70129941c3bc80b325b393702228b4
bach <bachtiaralif41@gmail.com>
2024-06-03 10:41:11 +0700

    feat: menambahkan routes reset-password dan reset-password-token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,7 @@ const { verifyTokenMiddleware } = require("../middleware/auth");
 // attendance Routes
 router.post("/login", authController.login);
 router.post("/logout", verifyTokenMiddleware, authController.logout);
+router.post("/reset-password", authController.resetPassword);
+router.post("/reset-password/token", authController.resetPasswordToken);
 
 module.exports = router;
