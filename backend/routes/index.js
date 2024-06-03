118a3fc27836304edb3cbf87e408059603c7c640
bach <bachtiaralif41@gmail.com>
2024-06-03 10:41:55 +0700

    feat: menggunakan middleware verifyToken pada routes employee dan admin

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
+const { verifyTokenMiddleware } = require("../middleware/auth");
 
 const admin = require("./admin/index");
 const employee = require("./employee/index");
 const authRoutes = require("./authRoutes");
 
 router.use("/auth", authRoutes);
-router.use("/employee", employee);
-router.use("/admin", admin);
+router.use("/employee", verifyTokenMiddleware, employee);
+router.use("/admin", verifyTokenMiddleware, admin);
 
 module.exports = router;
