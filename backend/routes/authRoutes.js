8910c94e4fc53ca3ac636f2999e7508f3ee7c75a
bach <bachtiaralif41@gmail.com>
2024-06-03 14:43:39 +0700

    fix: menghapus logout routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const { verifyTokenMiddleware } = require("../middleware/auth");
 
 // attendance Routes
 router.post("/login", authController.login);
-router.post("/logout", verifyTokenMiddleware, authController.logout);
 router.post("/reset-password", authController.resetPassword);
 router.post("/reset-password/token", authController.resetPasswordToken);
 
